cbed6219d46ff14a67f57915b7a5f7a35924337c
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2026-01-24 20:34:01 +0100

    form model change

--- vocabulary/infrastructure/models/word.py
+++ vocabulary/infrastructure/models/word.py
@@ -6,7 +6,7 @@ from django.db import models
 class Word(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.CharField(max_length=255)
-    context = models.CharField(max_length=255)
+    context = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
