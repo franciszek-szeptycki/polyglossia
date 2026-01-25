e9d2cd12905ca6885a87a486bc5876a49f990a9e
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2026-01-25 14:40:31 +0100

    paginate by 100

--- vocabulary/infrastructure/views/flashcard.py
+++ vocabulary/infrastructure/views/flashcard.py
@@ -7,7 +7,7 @@ class FlashcardListView(ListView):
     model = Flashcard
     template_name = "flashcards/list.html"
     context_object_name = "flashcards"
-    paginate_by = 20
+    paginate_by = 100
 
     def get_queryset(self):
         return super().get_queryset().filter(is_active=True)
