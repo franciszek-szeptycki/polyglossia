800c590b90db81bac1fd38f7cf18996d4817fd1d
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2026-03-14 00:52:29 +0100

    asyncronous creating falshcards 1.0

--- vocabulary/infrastructure/views/create_flashcards_from_word.py
+++ vocabulary/infrastructure/views/create_flashcards_from_word.py
@@ -1,3 +1,5 @@
+import threading
+from contextvars import copy_context
 from django.shortcuts import redirect
 from django.views import View
 
@@ -6,7 +8,12 @@ from vocabulary.infrastructure.factories.container import container
 
 class WordGenerateFlashcardsView(View):
     def dispatch(self, request, pk, *args, **kwargs):
+        ctx = copy_context()
 
-        container.use_case_generate_flashcards_for_word.execute(word_id=pk)
+        def run_task():
+            container.use_case_generate_flashcards_for_word.execute(word_id=pk)
+
+        thread = threading.Thread(target=ctx.run, args=(run_task,))
+        thread.start()
 
         return redirect(request.META.get("HTTP_REFERER", "/"))
