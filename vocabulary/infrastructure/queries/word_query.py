ecce2a7f068e4ed9961271c8664fe3a36eb05ff8
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2026-03-16 19:21:18 +0100

    next word - filter by profile

--- vocabulary/infrastructure/queries/word_query.py
+++ vocabulary/infrastructure/queries/word_query.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from profiles.infrastructure.middlewares import get_profile_id
 from vocabulary.infrastructure.models.word import Word
 
@@ -6,3 +8,8 @@ class WordQuery:
     @staticmethod
     def list():
         return Word.objects.filter(profile=get_profile_id())
+
+    @staticmethod
+    def get_next_word_without_flashcards() -> Optional[Word]:
+        words = Word.objects.filter(profile=get_profile_id()).order_by("created_at")
+        return next((w for w in words if not w.has_active_flashcards()), None)
--- vocabulary/infrastructure/views/word_next_redirect.py
+++ vocabulary/infrastructure/views/word_next_redirect.py
@@ -2,12 +2,12 @@ from django.shortcuts import redirect
 from django.views.generic import View
 
 from vocabulary.infrastructure.models.word import Word
+from vocabulary.infrastructure.queries.word_query import WordQuery
 
 
 class WordNextRedirectView(View):
     def get(self, request, *args, **kwargs):
-        words = Word.objects.all().order_by("created_at")  # i know, i know...
-        next_word = next((w for w in words if not w.has_active_flashcards), None)
+        next_word = WordQuery.get_next_word_without_flashcards()
 
         if next_word:
             return redirect("word_detail", pk=next_word.id)
