975512906072e2f34e5024cf9ad198d187641332
franciszek-szeptycki <franciszek.szeptycki@gmail.com>
2026-07-12 16:47:42 +0200

    logs

--- polyglossia/settings/prod.py
+++ polyglossia/settings/prod.py
@@ -6,6 +6,40 @@ import rollbar
 
 DEBUG = False
 
+LOGS_DIR = os.path.join(BASE_DIR, "logs")
+os.makedirs(LOGS_DIR, exist_ok=True)
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        },
+    },
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": os.path.join(LOGS_DIR, "app.log"),
+            "maxBytes": 10 * 1024 * 1024,  # 10 MB
+            "backupCount": 5,
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["file"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
+
 CSRF_TRUSTED_ORIGINS = [
     "https://polyglossia.tojest.dev",
 ]
